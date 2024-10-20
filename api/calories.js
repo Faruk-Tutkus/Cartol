bacd9671c815801ae3e6391dd56c38f4a28a2a8e
Faruk <97340877+Faruk-Tutkus@users.noreply.github.com>
2024-10-20 18:39:51 +0300

    Update calories.js

--- api/calories.js
+++ api/calories.js
@@ -1,19 +1,47 @@
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-core';
+import chrome from 'chrome-aws-lambda';
+
 export default async function handler(req, res) {
-  const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
-  });
-  const page = await browser.newPage();
-  await page.goto("http://quotes.toscrape.com/", {
-    waitUntil: "domcontentloaded",
-  });
-  const quotes = await page.evaluate(() => {
-    const quote = document.querySelector(".quote");
-    const text = quote.querySelector(".text").innerText;
-    const author = quote.querySelector(".author").innerText;
-    return { text, author };
-  });
-  console.log(quotes);
-  await browser.close();
+  let browser = null;
+
+  try {
+    // Chrome'u başlat
+    browser = await puppeteer.launch({
+      args: chrome.args,
+      executablePath: await chrome.executablePath,
+      headless: true, // Başsız modda çalıştır
+    });
+
+    const page = await browser.newPage();
+    await page.goto('https://www.diyetkolik.com/kac-kalori/beyaz-ekmek', {
+      waitUntil: 'networkidle2',
+    });
+
+    const protein = await page.evaluate(() => {
+      const element = document.querySelector(
+        '#solAnaSutun > div.p15.kurumsalBorder.backgroundWhite.mt-3 > div.mt-3 > div.d-flex.align-items-center.mt-2.carb-prot-fat > div:nth-child(3) > span.d-block.kkBigNumberIkincil.my-1 > span'
+      );
+      return element ? element.textContent.trim() : null;
+    });
+
+    const fat = await page.evaluate(() => {
+      const element = document.querySelector(
+        '#solAnaSutun > div.p15.kurumsalBorder.backgroundWhite.mt-3 > div.mt-3 > div.d-flex.align-items-center.mt-2.carb-prot-fat > div:nth-child(4) > span.d-block.kkBigNumberIkincil.my-1 > span'
+      );
+      return element ? element.textContent.trim() : null;
+    });
+
+    if (protein && fat) {
+      res.status(200).json({ calories: { protein, fat } });
+    } else {
+      res.status(404).json({ error: 'Kalori bilgisi bulunamadı.' });
+    }
+  } catch (error) {
+    console.error('Hata:', error);
+    res.status(500).json({ error: 'Sunucu hatası.' });
+  } finally {
+    if (browser) {
+      await browser.close(); // Tarayıcıyı kapat
+    }
+  }
 }
