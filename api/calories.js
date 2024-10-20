c378731e83f666dbbf7fdeed1d5cdc8c72786a9e
Faruk <97340877+Faruk-Tutkus@users.noreply.github.com>
2024-10-20 17:41:51 +0300

    Update calories.js

--- api/calories.js
+++ api/calories.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import fetch from 'node-fetch';
 import cheerio from 'cheerio';
 
 export default async function handler(req, res) {
@@ -9,22 +9,16 @@ export default async function handler(req, res) {
   }
 
   try {
-    // İlgili URL'yi oluştur
     const url = `https://www.diyetkolik.com/kac-kalori/${encodeURIComponent(food)}`;
-
-    // HTTP isteği yap
-    const response = await axios.get(url);
     
-    // HTML içeriğini cheerio ile yükle
-    const $ = cheerio.load(response.data);
+    const response = await fetch(url);
+    const body = await response.text();
+    
+    const $ = cheerio.load(body);
 
-    // Protein değeri için seçici
     const protein = $('#solAnaSutun > div.p15.kurumsalBorder.backgroundWhite.mt-3 > div.mt-3 > div.d-flex.align-items-center.mt-2.carb-prot-fat > div:nth-child(3) > span.d-block.kkBigNumberIkincil.my-1 > span').text().trim();
-
-    // Yağ değeri için seçici
     const fat = $('#solAnaSutun > div.p15.kurumsalBorder.backgroundWhite.mt-3 > div.mt-3 > div.d-flex.align-items-center.mt-2.carb-prot-fat > div:nth-child(4) > span.d-block.kkBigNumberIkincil.my-1 > span').text().trim();
 
-    // Eğer değerler varsa yanıt gönder
     if (protein && fat) {
       res.status(200).json({ calories: { protein, fat } });
     } else {
